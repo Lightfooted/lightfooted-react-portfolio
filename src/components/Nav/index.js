083a5e0666c6923578d770153613831a69a64341
Lightfooted <vesselofbalance@hotmail.com>
2021-11-02 22:42:10 -0400

    updated nav to make use of react-router-dom. complete

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,57 +1,30 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Nav(props) {
-  const {
-    categories = [],
-    setCurrentCategory,
-    contactSelected,
-    currentCategory,
-    setContactSelected,
-  } = props;
-
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
-
+function Nav() {
   return (
-    <header className="flex-row px-1">
-      <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="comp">💻</span>
-        </a>
-      </h2>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-              About me
-            </a>
-          </li>
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <span onClick={() => setContactSelected(true)}>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && 'navActive'
-                }`}
-              key={category.name}
-            >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setContactSelected(false);
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <header>
+      <div>
+        <nav>
+          <Link to='/' exact>
+            Home
+          </Link>
+          <Link to='/about'>
+            About
+          </Link>
+          <Link to='/project'>
+            Project
+          </Link>
+          <Link to='/modules'>
+            Modules
+          </Link>
+          <Link to='/contact'>
+            Contact
+          </Link>
+        </nav>
+      </div>
     </header>
-  );
+  )
 }
 
 export default Nav;
