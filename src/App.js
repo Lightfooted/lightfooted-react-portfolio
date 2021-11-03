753faaffb5a553f1ca6682603c4cb1d4de0b7c7d
Lightfooted <vesselofbalance@hotmail.com>
2021-11-02 22:36:26 -0400

    Reworked App.js to make use of react-router-dom. Complete

--- src/App.js
+++ src/App.js
@@ -1,50 +1,30 @@
-import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-import Nav from './components/Nav';
+import Home from './components/Home';
 import About from './components/About';
 import Project from './components/Project';
+import Modules from './components/Modules';
 import Contact from './components/Contact';
+import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 function App() {
-  const [categories] = useState([
-    {
-      name: 'projects',
-      description: 'List of my group projects',
-    },
-    { name: 'modules',
-      description: 'Assignments I have proudly completed' },
-  ]);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  const [contactSelected, setContactSelected] = useState(false);
-
   return (
+    <BrowserRouter>
     <div>
-      <Header></Header>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
-      <main>
-      {!contactSelected ? (
-          <>
-            <Project currentCategory={currentCategory}></Project>
-            <About></About>
-          </>
-        ) : (
-          <Contact></Contact>
-        )}
-      </main>
-      <Footer></Footer>
-    </div>
+    <Header/>
+    <Nav/>
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about'/>
+        <Route component={Project} path='/project'/>
+        <Route component={Modules} path='/modules'/>
+        <Route component={Contact} path='/contact'/>
+      </Switch>
+      <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-//TO DO: Conditionally render about me and project pages
