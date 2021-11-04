79ff0c5ace163c69f45901884516458141c956d6
Lightfooted <vesselofbalance@hotmail.com>
2021-11-04 04:57:02 -0400

    displayed projects and links out to their live pages. need to add descriptions

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import FirstGrpProject from '../../assets/projects/0.png'
 import SecondGrpProject from '../../assets/projects/1.png'
+import backgroundImage from '../../assets/background/gits-back.jpg'
 
 function Project() {
   return (
-    <section>
-      <h1>Halfstack Media</h1>
-    <img src={FirstGrpProject} alt="halfstack-media"/>
-      <h1>Pokedex Project</h1>
-    <img src={SecondGrpProject} alt="pokedex-project"/>
-    </section>
-  );
+    <main>
+      <img src={backgroundImage} style={{ width: "100%", height: "100%" }} alt="background" className="absolute"/>
+      <div className='relative flex p-20 mx-auto lg:pt-64'>
+        <section className='p-20 bg-black rounded-lg shadow-2xl bg-opacity-90 lg:flex'>
+            <a href="https://lverghese.github.io/project-one/"><img src={FirstGrpProject} className="my-2" style={{ width: "75% "}} alt="halfstack-media"/></a>
+          <a href="https://protected-hamlet-00069.herokuapp.com/"><img src={SecondGrpProject} className="my-2" style={{ width: "75% "}} alt="pokedex-project"/></a>
+        </section>
+        </div>
+    </main>
+  )
 }
 export default Project;
