75582ab08005ea5940016879fac3df6c51cd0603
Lightfooted <vesselofbalance@hotmail.com>
2021-11-04 04:49:51 -0400

    added links and made footer fixed. complete

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Icons from '../../assets/icons/gitlighticon.png';
+import GitHubIcon from '../../assets/icons/gitlighticon.png';
+import TwitIcon from '../../assets/icons/twittericon.png';
+import LinkIcon from '../../assets/icons/linkicon.png';
 
 function Footer() {
 
     return (
-        <div className="fixed-footer">
-        <footer id="footer" className="footer-container">
-        <a href="https://github.com/Lightfooted"><img src={Icons} alt="gitprofileicon"/></a>
+        <footer className='fixed inset-x-0 bottom-0 flex items-center p-4 bg-black justify-evenly'>
+        <a href="https://github.com/Lightfooted"><img src={GitHubIcon} alt="gitprofileicon"/></a>
+        <a href="https://twitter.com/Kim_Lightfooted"><img src={TwitIcon} alt="twitterprofileicon"/></a>
+        <a href="https://www.linkedin.com/in/kimberly-collazo-a12018225/"><img src={LinkIcon} alt="linkprofileicon"/></a>
         </footer>
-      </div>
     )
   }
   
   export default Footer;
-
-//TO DO: Add more links to footer
