dc6e66f7f6dc76a9b5e239ebda0fece8f470b4d3
Lightfooted <vesselofbalance@hotmail.com>
2021-11-04 04:51:52 -0400

    started stylizing nav. need to make it better

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <header>
-      <div>
+    <header className="bg-black">
+      <div className="container flex justify-between mx-auto">
         <nav>
-          <Link to='/' exact>
+          <Link to='/' exact className="items-center px-3 py-6 mr-4 text-4xl font-bold tracking-widest text-white inflex-flex hover:text-blue-500">
             Home
           </Link>
-          <Link to='/about'>
+          <Link to='/about' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white hover:text-blue-500'>
             About
           </Link>
-          <Link to='/project'>
+          <Link to='/project' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white hover:text-blue-500'>
             Project
           </Link>
-          <Link to='/modules'>
+          <Link to='/modules' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white hover:text-blue-500'>
             Modules
           </Link>
-          <Link to='/contact'>
+          <Link to='/contact' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white hover:text-blue-500'>
             Contact
           </Link>
         </nav>
