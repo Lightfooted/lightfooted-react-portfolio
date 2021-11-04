1f5621d8f621fc5eb5afee84c92b46eb4184f411
Lightfooted <vesselofbalance@hotmail.com>
2021-11-04 04:58:24 -0400

    need to make home page stand out more

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import backgroundImage from '../../assets/background/gits-back.jpg';
 
 function Home() {
 
   return (
-      <div>
-          <h1>Home</h1>
-      </div>
+      <main>
+        <img src={backgroundImage} style={{ width: "100%", height: "100%" }} alt="background" className="absolute"/>
+        <section className='relative flex justify-center min-h-screen px-8 pt-12 lg:pt-64'> 
+          <h1 className='text-4xl font-semibold text-white lg:leading-snug home-name'>Welcome to Lightfooted-Kimberly Collazo's Portfolio!</h1>
+        </section>
+      </main>
   )
 }
 
