2d970252fa115bec9ec5a425ae135a89d8d47e8c
Lightfooted <vesselofbalance@hotmail.com>
2021-11-05 01:18:06 -0400

    adjusted text, size and font

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,9 +6,9 @@ function Home() {
   return (
       <main>
         <img src={backgroundImage} style={{ width: "100%", height: "100%" }} alt="background" className="absolute"/>
-        <section className='relative flex justify-center min-h-screen px-8 pt-12 lg:pt-64'> 
-          <h1 className='text-4xl font-semibold text-white lg:leading-snug home-name'>Welcome to Lightfooted-Kimberly Collazo's Portfolio!</h1>
-        </section>
+          <section className='relative flex justify-center min-h-screen px-8 pt-12 lg:pt-64'> 
+            <h1 className='text-5xl text-white poppy lg:leading-snug home-name'>Welcome to Kimberly Collazo's Portfolio!</h1>
+          </section>
       </main>
   )
 }
