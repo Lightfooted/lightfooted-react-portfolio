0f07160b595bdc93fa817c92b73606911dbfb9cd
Lightfooted <vesselofbalance@hotmail.com>
2021-11-05 01:19:17 -0400

    added poppy for text in nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,19 +6,19 @@ function Nav() {
     <header className="bg-black">
       <div className="container flex justify-between mx-auto">
         <nav>
-          <Link to='/' exact className="items-center px-3 py-6 mr-4 text-4xl font-bold tracking-widest text-white inflex-flex hover:text-blue-500">
+          <Link to='/' exact className="items-center px-3 py-6 mr-4 text-4xl font-bold tracking-widest text-white poppy inflex-flex hover:text-blue-400">
             Home
           </Link>
-          <Link to='/about' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white hover:text-blue-500'>
+          <Link to='/about' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white poppy hover:text-blue-400'>
             About
           </Link>
-          <Link to='/project' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white hover:text-blue-500'>
+          <Link to='/project' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white poppy hover:text-blue-400'>
             Project
           </Link>
-          <Link to='/modules' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white hover:text-blue-500'>
-            Modules
+          <Link to='/résumé' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white poppy hover:text-blue-400'>
+            Résumé
           </Link>
-          <Link to='/contact' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white hover:text-blue-500'>
+          <Link to='/contact' className='inline-flex items-center px-3 py-3 my-6 font-semibold text-white poppy hover:text-blue-00'>
             Contact
           </Link>
         </nav>
