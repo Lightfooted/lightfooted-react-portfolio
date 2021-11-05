383bdd2acc18b4a0956cb871e1b4f60405f5927c
Lightfooted <vesselofbalance@hotmail.com>
2021-11-05 01:15:54 -0400

    made social links open in new tab when clicked

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,9 +7,9 @@ function Footer() {
 
     return (
         <footer className='fixed inset-x-0 bottom-0 flex items-center p-4 bg-black justify-evenly'>
-        <a href="https://github.com/Lightfooted"><img src={GitHubIcon} alt="gitprofileicon"/></a>
-        <a href="https://twitter.com/Kim_Lightfooted"><img src={TwitIcon} alt="twitterprofileicon"/></a>
-        <a href="https://www.linkedin.com/in/kimberly-collazo-a12018225/"><img src={LinkIcon} alt="linkprofileicon"/></a>
+        <a href="https://github.com/Lightfooted" rel='noopener noreferrer' target='_blank'><img src={GitHubIcon} alt="gitprofileicon"/></a>
+        <a href="https://twitter.com/Kim_Lightfooted" rel='noopener noreferrer' target='_blank'><img src={TwitIcon} alt="twitterprofileicon"/></a>
+        <a href="https://www.linkedin.com/in/kimberly-collazo-a12018225/" rel='noopener noreferrer' target='_blank'><img src={LinkIcon} alt="linkprofileicon"/></a>
         </footer>
     )
   }
