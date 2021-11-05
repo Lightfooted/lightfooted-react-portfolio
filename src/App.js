803f25819bf6ec9dc1fa9dc2a96f63b24144f57e
Lightfooted <vesselofbalance@hotmail.com>
2021-11-05 01:07:56 -0400

    changed modules to resume

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Project from './components/Project';
-import Modules from './components/Modules';
+import Résumé from './components/Résumé';
 import Contact from './components/Contact';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
@@ -16,7 +16,7 @@ function App() {
         <Route component={Home} path='/' exact />
         <Route component={About} path='/about'/>
         <Route component={Project} path='/project'/>
-        <Route component={Modules} path='/modules'/>
+        <Route component={Résumé} path='/Résumé'/>
         <Route component={Contact} path='/contact'/>
       </Switch>
       <Footer/>
