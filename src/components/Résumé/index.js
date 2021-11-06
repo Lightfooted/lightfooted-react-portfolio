ca5e597bbcfb534de77103dbb770cb1023e1f059
Lightfooted <vesselofbalance@hotmail.com>
2021-11-06 05:38:40 -0400

    stylized resume and added download link for my pdf

--- src/components/Résumé/index.js
+++ src/components/Résumé/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import BackgroundImage from '../../assets/background/gits-back.jpg';
+import BackImage from '../../assets/background/gits-back.jpg';
 import PDF from '../../assets/resume/resume.pdf';
 
 function Résumé() {
 
   return (
     <main>
-      <img src={BackgroundImage} style={{ width: "100%", height: "100%" }} alt="background" className="absolute"/>
+      <img src={BackImage} style={{ width: "100%", height: "100%" }} alt="background" className="absolute"/>
         <div className='relative flex justify-center p-20 mx-auto lg:pt-64'>
           <section className='p-10 bg-black rounded-lg shadow-2xl bg-opacity-80 lg:flex'>
               <div>
-                <h1 className='mb-2 text-5xl text-white'>Résumé</h1>
+                <h1 className='mb-3 text-5xl text-white poppy'>Résumé</h1>
                 <ul className='text-white list-disc poppy'>
-                <a href={PDF} rel='noreferrer' target="_blank" download>Résumé Download</a>
-                  <h1 className='underline'>Front-end Proficiencies</h1>
+                <a className='hover:text-blue-400' href={PDF} rel='noreferrer' target="_blank" download>Résumé Download</a>
+                  <h1 className='mt-2 mb-2 underline'>Front-end Proficiencies</h1>
                   <li>HTML</li>
                   <li>CSS</li>
                   <li>JavaScript</li>
@@ -21,7 +21,7 @@ function Résumé() {
                   <li>Bootstrap</li>
                   <li>Tailwind</li>
                   <li>Version Control</li>
-                  <h1 className='underline'>Back-end Proficiencies</h1>
+                  <h1 className='mt-2 mb-2 underline'>Back-end Proficiencies</h1>
                   <li>Express</li>
                   <li>Node</li>
                   <li>APIs</li>
