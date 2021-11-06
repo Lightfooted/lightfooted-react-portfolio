e4f472de62cf207997b8b31bb6bca81556bf4aed
Lightfooted <vesselofbalance@hotmail.com>
2021-11-06 05:33:10 -0400

    styled contact page, added a link to my email and phone number, decided to remove sizing of textarea and added a character limit of 150

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import BackImage from '../../assets/background/gits-back.jpg';
 
 import { validateEmail } from '../../utils/helpers';
 
@@ -34,30 +35,42 @@ function Contact() {
   };
 
   return (
-    <section>
-      <h1 data-testid="h1tag">Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
+    <main>
+      <img src={BackImage} style={{ width: '100%', height: '100%' }} alt='background' className='absolute'/>
+        <div className='relative flex justify-center p-20 mx-auto lg:pt-64'>
+          <section className='p-20 text-white bg-black rounded-lg shadow-2xl poppy bg-opacity-80 lg:flex'>
+          <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+          <label>Name: </label>
+          <input className='mb-3 text-black' type='text' name='name' defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+          <label>Email address: </label>
+          <input className='mb-3 text-black' type='email' name='email' defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+          <label>Message: </label>
+          <textarea maxLength='150' className='text-black resize-none' name='message' defaultValue={message} onBlur={handleChange} />
+        </div>
+        <div>
+        <label></label>
+        <a href="mailto:vesselofbalance@hotmail.com" className='hover:text-blue-400'>Click here for Kimberly's email</a>
+        </div>
+        <div>
+        <label></label>
+        <a href="tel:+4074840907" className='hover:text-blue-400'>Click here for Kimberly's phone number</a>
         </div>
         {errorMessage && (
           <div>
-            <p className="error-text">{errorMessage}</p>
+            <p>{errorMessage}</p>
           </div>
         )}
-        <button data-testid="button" type="submit">Submit</button>
+        <button type='submit' className='h-10 px-5 mt-3 text-white transition-colors duration-150 border rounded-lg focus:shadow-outline hover:bg-gray-700 hover:text-white'>Submit</button>
       </form>
-    </section>
-  );
+          </section>
+        </div>
+    </main>
+  )
 }
 
 export default Contact;
