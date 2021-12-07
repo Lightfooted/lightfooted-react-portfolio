281c5f439c4c4be1f2f51f695d1a667f20c46f0f
Lightfooted <vesselofbalance@hotmail.com>
2021-12-07 03:55:28 -0500

    got the grid to position the way I wanted, centered project text, changed project image to link to live application or video and also added a separate link to github repo. need to redo screenshots so that they are all the same height and width, would like to add a border around the images and have a pop out hover in the future.

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,19 +6,41 @@ import NetAPI from '../../assets/projects/3.png'
 import README from '../../assets/projects/4.png'
 import Password from '../../assets/projects/5.png'
 import LastGrpProject from '../../assets/projects/6.png'
+import GitLink from '../../assets/icons/github-2-24.png'
 
 function Project() {
   return (
     <main>
-      <div className='relative flex justify-center p-20 mx-auto lg:pt-24'>
-        <div class="rounded-lg shadow-2xl grid grid-cols-4 gap-5 bg-black text-white bg-opacity-80 p-10">
-          <div href='https://lverghese.github.io/project-one/' rel='noopener noreferrer' target='_blank'>First Group Project: Halfstack-Media<img src={FirstGrpProject} className=' justify-evenly' alt='halfstack-media'/></div>
-          <div href='https://protected-hamlet-00069.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Second Group Project: Pokedex-Project<img src={SecondGrpProject} className=' justify-evenly' alt='pokedex-project'/></div>
-          <div href='https://high-tech-match.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Final Group Project: High Tech Match Love<img src={LastGrpProject} className=' justify-evenly' alt='final-project'/></div>
-          <div href='https://github.com/Lightfooted/password-generator' rel='noopener noreferrer' target='_blank'>Module Assignment: Password Generator<img src={Password} className=' justify-evenly' alt='pass'/></div>
-          <div href='https://github.com/Lightfooted/professional-README-generator' rel='noopener noreferrer' target='_blank'>Module Assignment: README Generator<img src={README} className=' justify-evenly' alt='gen'/></div>
-          <div href='https://github.com/Lightfooted/Social-Network-API' rel='noopener noreferrer' target='_blank'>Module Assignment: Social Network API<img src={NetAPI} className=' justify-evenly' alt='n-api'/></div>
-          <div href='https://github.com/Lightfooted/Budget-Tracker' rel='noopener noreferrer' target='_blank'>Module Assignment: Offline Budget Tracker<img src={BudgetAssignment} className=' justify-evenly' alt='budget-tracker'/></div>
+      <div className='relative flex justify-center p-20 mx-auto lg:pt-18'>
+        <div className="grid grid-cols-4 gap-5 p-10 text-center text-white bg-black rounded-lg shadow-2xl bg-opacity-80">
+          <div>
+            <a href='https://lverghese.github.io/project-one/' rel='noopener noreferrer' target='_blank'>First Group Project: Halfstack-Media<img src={FirstGrpProject} alt='halfstack-media'/></a>
+            <a href='https://github.com/lverghese/project-one' rel='noopener noreferrer' target='_blank'><img src={GitLink} className='mx-auto mt-2' alt='gitlink'/></a>
+            </div>
+          <div>
+            <a href='https://protected-hamlet-00069.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Second Group Project: Pokedex-Project<img src={SecondGrpProject} alt='pokedex-project'/></a>
+            <a href='https://github.com/Lightfooted/pokedex-project' rel='noopener noreferrer' target='_blank'><img src={GitLink} className='mx-auto mt-2' alt='gitlink'/></a>
+            </div>
+          <div>
+            <a href='https://high-tech-match.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Final Group Project: High Tech Match Love<img src={LastGrpProject} alt='final-project'/></a>
+            <a href='https://github.com/Lightfooted/High-Tech-Match-Love' rel='noopener noreferrer' target='_blank'><img src={GitLink} className='mx-auto mt-2' alt='gitlink'/></a>
+            </div>
+          <div>
+            <a href='https://lightfooted.github.io/password-generator/' rel='noopener noreferrer' target='_blank'>Module Assignment: Password Generator<img src={Password} alt='pass'/></a>
+            <a href='https://github.com/Lightfooted/password-generator' rel='noopener noreferrer' target='_blank'><img src={GitLink} className='mx-auto mt-2' alt='gitlink'/></a>
+            </div>
+          <div>
+            <a href='https://www.youtube.com/watch?v=HzrT7pC8dTw' rel='noopener noreferrer' target='_blank'>Module Assignment: README Generator<img src={README} alt='gen'/></a>
+            <a href='https://github.com/Lightfooted/professional-README-generator' rel='noopener noreferrer' target='_blank'><img src={GitLink} className='mx-auto mt-2' alt='gitlink'/></a>
+            </div>
+          <div>
+            <a href='https://www.youtube.com/watch?v=2tp7mEg7tNU' rel='noopener noreferrer' target='_blank'>Module Assignment: Social Network API<img src={NetAPI} alt='n-api'/></a>
+            <a href='https://github.com/Lightfooted/Social-Network-API' rel='noopener noreferrer' target='_blank'><img src={GitLink} className='mx-auto mt-2' alt='gitlink'/></a>
+            </div>
+          <div>
+            <a href='https://agile-scrubland-63874.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Module Assignment: Offline Budget Tracker<img src={BudgetAssignment} alt='budget-tracker'/></a>
+            <a href='https://github.com/Lightfooted/Budget-Tracker' rel='noopener noreferrer' target='_blank'><img src={GitLink} className='mx-auto mt-2' alt='gitlink'/></a>
+            </div>
         </div>
       </div>
     </main>
