2549fd9ebd0fe21cb4aa0300d6a4261a672eb59e
Lightfooted <vesselofbalance@hotmail.com>
2021-12-07 00:16:49 -0500

    work in progress: redid the grid for projects, removed img style as it was no longer necessary. need to put the hrefs in an a tag within the divs

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -10,17 +10,17 @@ import LastGrpProject from '../../assets/projects/6.png'
 function Project() {
   return (
     <main>
-      <div className='relative flex mx-auto lg:pt-32'>
-        <div className="container p-10 pl-24 mx-auto mb-20 text-white bg-black rounded-lg shadow-2xl poppy lg:grid lg:grid-cols-3 bg-opacity-80">
-        <a href='https://high-tech-match.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Final Group Project: High Tech Match Love<img src={LastGrpProject} className=' justify-evenly' style={{ width: '80% '}} alt='final-project'/></a>
-        <a href='https://protected-hamlet-00069.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Second Group Project: Pokedex-Project<img src={SecondGrpProject} className=' justify-evenly' style={{ width: '80% '}} alt='pokedex-project'/></a>
-        <a href='https://lverghese.github.io/project-one/' rel='noopener noreferrer' target='_blank'>First Group Project: Halfstack-Media<img src={FirstGrpProject} className=' justify-evenly' style={{ width: '80% '}} alt='halfstack-media'/></a>
-        <a href='https://github.com/Lightfooted/Budget-Tracker' rel='noopener noreferrer' target='_blank'>Module Assignment: Offline Budget Tracker<img src={BudgetAssignment} className=' justify-evenly' style={{ width: '80% '}} alt='budget-tracker'/></a>
-        <a href='https://github.com/Lightfooted/Social-Network-API' rel='noopener noreferrer' target='_blank'>Module Assignment: Social Network API<img src={NetAPI} className=' justify-evenly' style={{ width: '80% '}} alt='n-api'/></a>
-        <a href='https://github.com/Lightfooted/professional-README-generator' rel='noopener noreferrer' target='_blank'>Module Assignment: README Generator<img src={README} className=' justify-evenly' style={{ width: '80% '}} alt='gen'/></a>
-        <a href='https://github.com/Lightfooted/password-generator' rel='noopener noreferrer' target='_blank'>Module Assignment: Password Generator<img src={Password} className=' justify-evenly' style={{ width: '80% '}} alt='pass'/></a>
+      <div className='relative flex justify-center p-20 mx-auto lg:pt-24'>
+        <div class="rounded-lg shadow-2xl grid grid-cols-4 gap-5 bg-black text-white bg-opacity-80 p-10">
+          <div href='https://lverghese.github.io/project-one/' rel='noopener noreferrer' target='_blank'>First Group Project: Halfstack-Media<img src={FirstGrpProject} className=' justify-evenly' alt='halfstack-media'/></div>
+          <div href='https://protected-hamlet-00069.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Second Group Project: Pokedex-Project<img src={SecondGrpProject} className=' justify-evenly' alt='pokedex-project'/></div>
+          <div href='https://high-tech-match.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Final Group Project: High Tech Match Love<img src={LastGrpProject} className=' justify-evenly' alt='final-project'/></div>
+          <div href='https://github.com/Lightfooted/password-generator' rel='noopener noreferrer' target='_blank'>Module Assignment: Password Generator<img src={Password} className=' justify-evenly' alt='pass'/></div>
+          <div href='https://github.com/Lightfooted/professional-README-generator' rel='noopener noreferrer' target='_blank'>Module Assignment: README Generator<img src={README} className=' justify-evenly' alt='gen'/></div>
+          <div href='https://github.com/Lightfooted/Social-Network-API' rel='noopener noreferrer' target='_blank'>Module Assignment: Social Network API<img src={NetAPI} className=' justify-evenly' alt='n-api'/></div>
+          <div href='https://github.com/Lightfooted/Budget-Tracker' rel='noopener noreferrer' target='_blank'>Module Assignment: Offline Budget Tracker<img src={BudgetAssignment} className=' justify-evenly' alt='budget-tracker'/></div>
         </div>
-        </div>
+      </div>
     </main>
   )
 }
