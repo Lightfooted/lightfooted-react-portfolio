af029f4eefa7c8077a0fbb31790ec9ff73070065
Lightfooted <vesselofbalance@hotmail.com>
2021-12-07 00:11:47 -0500

    updated About me description, image and style of page.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,12 +5,15 @@ function About() {
 
   return (
     <main>
-        <div className='relative flex p-20 mx-auto lg:pt-48'>
-          <section className='p-10 bg-black rounded-lg shadow-2xl bg-opacity-80 lg:flex'>
-            <img src={coverImage} style={{ width: '15%' }} alt='bio' className='mr-6 rounded lg:w-64 lg:64'/>
+        <div className='relative flex justify-center w-auto p-20 lg:pt-48'>
+          <section className='p-10 bg-black rounded-lg shadow-2xl lg:flex bg-opacity-80'>
+            <img src={coverImage} alt='bio' className='mr-6 rounded lg:flex'/>
               <div>
                 <h1 className='mb-5 text-6xl text-white underline poppy'>Kimberly Collazo</h1>
-                <p className='text-2xl text-white poppy'>Hi! I'm Kim! Nice to meet you! I'm a Full-Stack Web Developer. I attended the UCF Boot-Camp in June of 2021. It's been quite a journey! My interests are video gaming, anime, most genres of music and coding in my free time.</p>
+                <p className='text-2xl text-white poppy'>Nice to meet you! I'm Kim! I'm a Full-Stack Web Developer.</p>
+                <p className='text-2xl text-white poppy'>I attended the UCF Boot-Camp in June of 2021 and graduated on November 2021. It's been quite a journey!</p>
+                <p className='text-2xl text-white poppy'>I'm interested in building a career in Web Development and eventually dabbling into Video Game Development. </p>
+                <p className='text-2xl text-white poppy'>My hobbies are video gaming, anime, listening to most genres of music and coding for fun in my free time.</p>
               </div>
           </section>
         </div>
