c5bc27131f80ee15bc2608132267f762375f4ecc
Lightfooted <vesselofbalance@hotmail.com>
2021-12-17 23:34:25 -0500

    outline rule

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      outline: {
+        white: '1px solid #ffffff'
+      }
+    },
   },
   variants: {
     extend: {},
