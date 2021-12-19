b00fe0a1c0b58719199d43a85b1697c4f32de50b
Lightfooted <vesselofbalance@hotmail.com>
2021-12-19 00:10:20 -0500

    added loader to give decrypt time to start once the page fully loads.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDencrypt } from 'use-dencrypt-effect';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 const values = ["Hello!", "My name is Kimberly Collazo.", "AKA Lightfooted.", "Welcome to my portfolio."];
 
 const decryption = {
   chars: `人 生 今 時 力 新 部 八 九 保 同 方 強 自 正 理 
           愛 遊 勇 因 美 器 敬 永 選 治 賢 固 希 喜 軍 航`
-    .replace(""),
+    .replace(''),
   interval: 70
 };
 
@@ -29,8 +31,9 @@ function Home() {
 
   return (
       <main>
-          <section className='relative flex justify-center p-20 mx-auto overflow-hidden lg:pt-64'> 
-            <h1 className='text-5xl text-white text-shadow poppy lg:leading-snug home-name'>{result}</h1>
+          <section className='relative flex justify-center p-20 mx-auto overflow-hidden lg:pt-64'>
+              <Loader type='Puff' color='#ffffff' height={100} width={100} timeout={4000} />
+            <h1 className='text-5xl text-white text-shadow poppy lg:leading-snug'>{result}</h1>
           </section>
       </main>
   )
